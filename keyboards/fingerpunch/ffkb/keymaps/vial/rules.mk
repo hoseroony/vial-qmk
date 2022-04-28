87c9ccae18f90ef03580c51dbeccac70ba92bdea
hoseroony <mike.kinsman@gmail.com>
2022-04-27 20:48:56 -0700

    commenting out casemodes here too

--- keyboards/fingerpunch/ffkb/keymaps/vial/rules.mk
+++ keyboards/fingerpunch/ffkb/keymaps/vial/rules.mk
@@ -1,13 +1,13 @@
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
-#VIAL_INSECURE = yes
+VIAL_INSECURE = yes
 VIAL_ENCODERS_ENABLE = yes
 LTO_ENABLE = yes
 QMK_SETTINGS = no
 TAP_DANCE_ENABLE = no
-CASEMODES_ENABLE = yes
+CASEMODES_ENABLE = no
 
-ifeq ($(strip $(CASEMODES_ENABLE)), yes)
-	OPT_DEFS += -DCASEMODES_ENABLE
-    SRC += users/sadekbaroudi/casemodes.c
-endif
+#ifeq ($(strip $(CASEMODES_ENABLE)), yes)
+#	OPT_DEFS += -DCASEMODES_ENABLE
+#   SRC += users/sadekbaroudi/casemodes.c
+#endif
